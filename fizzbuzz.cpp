2b0f441969a28283c64172dcc27e0e7fd8eaa14c
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2024-01-12 15:29:38 +0100

    +variable length, +exit holder

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
 int main() {
-    for (int i = 1; i < 100; i++) {
+    int x;
+
+    std::cout << "Enter a number: ";
+    std::cin >> x;
+
+    for (int i = 1; i < (x+1); i++) {
         if (!(i % 3) && !(i % 5)) {
             std::cout << "FizzBuzz\n";
         } else if (!(i % 5)) {
@@ -12,5 +17,10 @@ int main() {
             std::cout << i << "\n";
         }
     }
+
+    char exit;
+    std::cout << "Input any character to exit: ";
+    std::cin >> exit;
+
     return 0;
 }
